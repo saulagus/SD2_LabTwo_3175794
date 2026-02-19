d7bedbfc0fef3e00c94cad3219e872e2887a65c2
saulagus <genriquezagus@gmail.com>
2026-02-19 14:49:13 -0600

    Added Stub methods to Conversions file

--- src/Conversions.java
+++ src/Conversions.java
@@ -1,3 +1,22 @@
 public class Conversions {
 
+    public double euroToDollar(double euro) {
+        return 0.0;
+    }
+
+    public double dollarToEuro(double dollar) {
+        return 0.0;
+    }
+
+    public int stringToInteger(String val) {
+        return 0;
+    }
+
+    public String integerToString(int val) {
+        return "";
+    }
+
+    public String switchCase(String val) {
+        return "";
+    }
 }
